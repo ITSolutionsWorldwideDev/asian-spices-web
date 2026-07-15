b5f7988efaa59d849376c835cda625b902a56254
Mahvia <mahviasajjadabbasi@gmail.com>
2026-07-15 16:33:51 +0500

    resolving conflicts

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -26,7 +26,7 @@ const ContactUs = () => {
             <h1 className="text-4xl md:text-5xl font-bold mb-4">Get In Touch</h1>
             <p className="text-base md:text-lg max-w-2xl mx-auto text-white/90 leading-relaxed">
               Have a question? We&apos;d love to hear from you. Send us a message
-              and we&apos;ll respond as soon as possible.
+              and we&apos;ll respond as soon as possible..
             </p>
           </div>
         </div>
