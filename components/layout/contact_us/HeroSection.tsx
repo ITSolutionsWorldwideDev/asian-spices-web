4da1312aaa777f278a039fbe422984a511fc6fba
Ahmed Nadeem <ahmed962456@gmail.com>
2026-09-11 15:35:57 +0500

    changes by ahmed nadeem

--- components/layout/contact_us/HeroSection.tsx
+++ components/layout/contact_us/HeroSection.tsx
@@ -1,13 +1,11 @@
 import ContactUsForm from "./ContactUsForm";
-import Faqs from "./Faqs";
 
 export default function HeroSection() {
   return (
     <div className="bg-[#f5f0ea] py-12 md:py-16 px-4 sm:px-6 lg:px-8">
       <div className="container mx-auto">
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 lg:gap-12 items-start">
+        <div className="max-w-2xl mx-auto">
           <ContactUsForm />
-          <Faqs />
         </div>
 
         {/* Company Details */}
